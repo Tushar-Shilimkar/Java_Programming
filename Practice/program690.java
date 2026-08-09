634c2f8726e9072d80a2de6431f1a82822dadff0
Tushar Vijay Shilimkar <144754871+Tushar-Shilimkar@users.noreply.github.com>
2026-08-09 07:44:03 +0530

    Add files via upload

---
+++ Practice/program690.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.util.Scanner;
+
+class program690
+{
+    public static void main(String A[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+        String FolderName = null;
+        String PackFileName = null;
+
+        System.out.println("Enter Folder name : ");
+        FolderName = sobj.nextLine();
+
+        System.out.println("Enter the name of packed File : ");
+        PackFileName = sobj.nextLine();
+
+        File fobjfolder = new File(FolderName);
+
+        if((fobjfolder.exists()) && (fobjfolder.isDirectory()))
+        {
+            System.out.println("Folder exists");
+
+            File fobjpack = new File (PackFileName);
+            fobjpack.createNewFile();       // Pack file gets Created
+
+            File fArr[] = fobjfolder.listFiles();
+
+            System.out.println("Number of files in folder : "+fArr.length);
+
+            for(int i = 0; i < fArr.length; i++)
+            {
+                // Logic
+            }
+        }
+        else
+        {
+            System.out.println("There is no such folder");
+        }
+    }
+}
